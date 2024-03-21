bf888aeaf280056a25a59cb55cbfd0a8eec57966
iyab313 <160235223+iyab313@users.noreply.github.com>
2024-03-21 21:00:29 +0900

    dd

---
+++ script/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('.nav-opn button').click(function(){
+        $('nav').animate({left: 0}, 'slow'); 
+    });
+
+    $('.nav-cls button').click(function(){
+        $('nav').animate({left: '-100%'}, 'slow'); 
+    });
+});
